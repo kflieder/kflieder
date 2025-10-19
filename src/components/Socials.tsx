0e71df4141fc7d77b611c6f2c8f785d59e4ca3ac
Kacey <125841074+kflieder@users.noreply.github.com>
2025-10-18 21:14:07 -0600

    Update metadata and social links
    Enhanced site metadata in layout.tsx for SEO and social sharing, including Open Graph and Twitter details. Updated Socials.tsx to use correct external profile links and open them in new tabs. Updated favicon.ico.

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Socials() {
   return (
     <div className='flex gap-4 mt-4 sm:justify-start justify-center'>
-      <a href="" aria-label='Facebook Link'>
+      <a target='_blank' href="https://www.facebook.com/kflieder" aria-label='Facebook Link'>
         <svg className='text-white/70 hover:text-white/100 transition-colors fill-current h-16 w-16' role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Facebook</title><path d="M9.101 23.691v-7.98H6.627v-3.667h2.474v-1.58c0-4.085 1.848-5.978 5.858-5.978.401 0 .955.042 1.468.103a8.68 8.68 0 0 1 1.141.195v3.325a8.623 8.623 0 0 0-.653-.036 26.805 26.805 0 0 0-.733-.009c-.707 0-1.259.096-1.675.309a1.686 1.686 0 0 0-.679.622c-.258.42-.374.995-.374 1.752v1.297h3.919l-.386 2.103-.287 1.564h-3.246v8.245C19.396 23.238 24 18.179 24 12.044c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.628 3.874 10.35 9.101 11.647Z"/></svg>
       </a>
-      <a href='' aria-label='GitHub Link'>
+      <a target='_blank' href='https://www.github.com/kflieder' aria-label='GitHub Link'>
         <svg className='w-16 h-16 text-white/70 hover:text-white/100 transition-colors fill-current' role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
         </a>
-        <a href='' aria-label='LinkedIn Link'>
+        <a href='https://www.linkedin.com/in/kflieder' aria-label='LinkedIn Link' target='_blank' rel='noopener noreferrer'>
             <svg className="w-18 h-18 text-white/70 hover:text-white/100 transition-colors fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path d="M26.111,3H5.889c-1.595,0-2.889,1.293-2.889,2.889V26.111c0,1.595,1.293,2.889,2.889,2.889H26.111c1.595,0,2.889-1.293,2.889-2.889V5.889c0-1.595-1.293-2.889-2.889-2.889ZM10.861,25.389h-3.877V12.87h3.877v12.519Zm-1.957-14.158c-1.267,0-2.293-1.034-2.293-2.31s1.026-2.31,2.293-2.31,2.292,1.034,2.292,2.31-1.026,2.31-2.292,2.31Zm16.485,14.158h-3.858v-6.571c0-1.802-.685-2.809-2.111-2.809-1.551,0-2.362,1.048-2.362,2.809v6.571h-3.718V12.87h3.718v1.686s1.118-2.069,3.775-2.069,4.556,1.621,4.556,4.975v7.926Z"></path></svg>
         </a>
     </div>
