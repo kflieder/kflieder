163cb384f289ec7d1f6756fa782c85d56c83c291
Kacey <125841074+kflieder@users.noreply.github.com>
2025-11-08 08:32:00 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-import { type NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimentalturbopack: false,
   transpilePackages: ["@sanity/client", "sanity"],
   webpack(config) {
     config.module.rules.push({
